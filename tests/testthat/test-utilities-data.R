93ad8596577cbe805446bb1fcbbf84a030126e1a
KatrinCoboeken <katrin.coboeken@bayer.com>
2025-11-27 07:00:10 +0000

    correct typo

--- tests/testthat/test-utilities-data.R
+++ tests/testthat/test-utilities-data.R
@@ -1,4 +1,4 @@
-# testProject was set up by setup.R, this provide variable projectconfiguration and test data
+# testProject was set up by setup.R, this provides variable projectconfiguration and test data
 
 dataObserved <- readObservedDataByDictionary(projectConfiguration)
 
@@ -26,29 +26,29 @@ test_that("It should filter data by fileIds parameter", {
     sheetName = "DataFiles",
     skipDescriptionRow = TRUE
   )
-  
+
   # Get available file identifiers
   availableFileIds <- unique(dataList$fileIdentifier)
-  
+
   # Test that we can filter by a subset of fileIds
   if (length(availableFileIds) > 1) {
     # Select first fileId
     selectedFileId <- availableFileIds[1]
-    
+
     dataObservedFiltered <- readObservedDataByDictionary(
       projectConfiguration,
       fileIds = selectedFileId
     )
-    
+
     # Should return a valid data.table
-    expect_true(data.table::is.data.table(dataObservedFiltered), 
+    expect_true(data.table::is.data.table(dataObservedFiltered),
                 "Filtered data should be a data table")
-    
+
     # Should have fewer rows than the full dataset
     expect_lt(nrow(dataObservedFiltered), nrow(dataObserved),
               label = "Filtered data should have fewer rows than unfiltered data")
   }
-  
+
   # Test that passing invalid fileIds raises an error
   expect_error(
     readObservedDataByDictionary(
